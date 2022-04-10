4f02914dea4b8fd385a39a8707d43d5bd0fb4c17
frappe <frappe@ubuntu.vm>
2022-04-10 20:26:52 +0000

    Add transaction doctype

---
+++ archiving_system/archiving_system/doctype/transaction/transaction.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Samia Saba And Rowan and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Transaction', {
+	// refresh: function(frm) {
+
+	// }
+});
